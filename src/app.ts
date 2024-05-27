b34d611f8e1d39e9988165a206c82e2b090d0820
miavai649 <mahmudulhaquenoor@gmail.com>
2024-05-28 04:05:08 +0600

    routes code repetation reduced

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import { StudentRoutes } from './app/modules/student/student.route';
 import { UserRoutes } from './app/modules/user/user.route';
 import globalErrorHandler from './app/middleware/globalErrorHandler';
 import notFound from './app/middleware/notFound';
+import router from './app/routes';
 
 const app: Application = express();
 
@@ -14,16 +15,15 @@ app.use(express.json());
 app.use(cors());
 
 // application routes
-app.use('/api/v1/students', StudentRoutes);
-app.use('/api/v1/users', UserRoutes);
+app.use('/api/v1', router);
 
-const getAController = (req: Request, res: Response) => {
+const test = (req: Request, res: Response) => {
   const a = 'Hello World!';
 
   res.send(a);
 };
 
-app.get('/', getAController);
+app.get('/', test);
 
 app.use(globalErrorHandler);
 
