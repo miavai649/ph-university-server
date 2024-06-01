39628738075a82c7f092abdedf24dee2a88808f8
miavai649 <mahmudulhaquenoor@gmail.com>
2024-06-01 15:59:12 +0600

    bug fixed in generate student id function

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -12,12 +12,28 @@ const findLastStudentId = async () => {
       sort: { createdAt: -1 },
     },
   ).lean();
-  return lastStudent?.id ? lastStudent?.id.substring(6) : undefined;
+  return lastStudent?.id ? lastStudent?.id : undefined;
 };
 
 // generating student id (year + semesterCode + 4 digit number)
 export const generateStudentId = async (payload: TAcademicSemester) => {
-  const currentId = (await findLastStudentId()) || (0).toString();
+  let currentId = (0).toString();
+
+  // 2030 01 0001
+  const lastStudentId = await findLastStudentId();
+  const lastStudentSemesterYear = lastStudentId?.substring(0, 4);
+  const lastStudentSemesterCode = lastStudentId?.substring(5, 6);
+  const currentStudentSemesterYear = payload.year;
+  const currentStudentSemesterCode = payload.code;
+
+  if (
+    lastStudentId &&
+    lastStudentSemesterCode === currentStudentSemesterCode &&
+    lastStudentSemesterYear === currentStudentSemesterYear
+  ) {
+    currentId = lastStudentId?.substring(6);
+  }
+
   let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
 
   incrementId = `${payload.year}${payload.code}${incrementId}`;
