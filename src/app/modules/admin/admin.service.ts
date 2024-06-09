48848ef7fa4e744e37030b40e3cd48ef240c7972
miavai649 <mahmudulhaquenoor@gmail.com>
2024-06-09 16:18:33 +0600

    get all admin api created

---
+++ src/app/modules/admin/admin.service.ts
@@ -0,0 +1,20 @@
+import QueryBuilder from '../../builder/QueryBuilder';
+import { AdminSearchableFields } from './admin.constant';
+import { Admin } from './admin.model';
+
+const getAllAdminFromDb = async (query: Record<string, unknown>) => {
+  const adminQuery = new QueryBuilder(Admin.find(), query)
+    .search(AdminSearchableFields)
+    .filter()
+    .sort()
+    .paginate()
+    .fields();
+
+  const result = await adminQuery.modelQuery;
+
+  return result;
+};
+
+export const AdminServices = {
+  getAllAdminFromDb,
+};
